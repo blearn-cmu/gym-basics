8ba31a6a362129b694e4b5a8f9b4a2f692154c8f
learnb <bryan@bryanlearn.com>
2018-07-22 21:12:24 -0400

    unfinished

---
+++ cartpole-random.py
@@ -0,0 +1,29 @@
+import gym
+import numpy as np
+
+env = gym.make("CartPole-v0")
+
+def run_episode(env, parameters):  
+    observation = env.reset()
+    totalreward = 0
+    for _ in range(200):
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        totalreward += reward
+        if done:
+            break
+    return totalreward
+
+bestparams = None  
+bestreward = 0  
+for episode in range(10000):  
+    parameters = np.random.rand(4) * 2 - 1
+    reward = run_episode(env,parameters)
+    if reward > bestreward:
+        bestreward = reward
+        bestparams = parameters
+        # considered solved if the agent lasts 200 timesteps
+        if reward == 200:
+            break
+
+print("Number of episodes: {}".format(episode))
